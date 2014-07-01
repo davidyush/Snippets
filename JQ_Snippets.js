3458219f1d9a330e2ff0a79e635ed648f0c54a63
fleple <davidyushkov@gmail.com>
2014-07-01 12:04:37 +0400

    another 5kb of jq

--- JQ_Snippets.js
+++ JQ_Snippets.js
@@ -303,26 +303,165 @@ pluton 0.187
       }
    });
 </script>
+------------------------------------------------------------------
 
 
+$(window).on({
+	resize : function () {
+		$('#resize-text').text("Width: " + window.innerWidth + ", Height: " + window.innerHeight);
+	},
+	scroll : function () {
+		$("#scroll-text").text('Position Y:' + window.scrollY);
+		console.log(e.data.user);// Test string
+	}
+}, {user: "Test string"});
 
 
+//Together!
+$(window).on("resize scroll", {user: "Some String"}, function(e) {
+	$("#resize-text").text("Width: " + window.innerWidth + ", Height: " + window.innerHeight);
+	$("#scroll-text").text('Position Y:' + window.scrollY);
+	console.log(e.data.user);
+});
+
+//отключать события
+
+$(window).off();//все события окна
+$(window).off('resize');//scroll останеться
+
+//если вместо on написать one, то событие произойдет лишь один раз
+
+можно инициировать событие 
+
+$("button").trigger('click');
+таким образом, если написать
+$(window).trigger('resize') - то событие сразу вызоветься но и работать также будет.
+-------------------------------------------------------
+манипуляции
+
+создание новых узлов dom
+$('<img>',{
+	src: "img/jq.jpg",
+	alt: "something",
+	title: "Zorro!",
+	click: function(e) {
+		$(this).toggleClass("selectedImg");
+	}
+/*	on: {
+		click: function(e) {
+
+		},
+	}*/
+})
+.css({
+	"padding" : "10px",
+	"border": "1px solid black"
+})
+.appendTo('body');
 
 
+разнообразная вставка:
+
+обернуть данный селектор:
+ul.list>li*4>a[href="#"]{something}
+
+$('.list').wrap("<div></div>")
+
+обернуть все дочерние элементы
+$('.list').wrapInner("<div></div>");
+
+wrappAll - попытаеться собрать все элементы в доме и обернуть их.
+
+appendTo('') - вставить последним ребенком выбраного узла
+prependTo('') - вставить первым ребенком выбраного узла
+
+after - вставка после элемента
+insertAfter('selector')
+
+before - вставка после элемент
+insertBefore('selector') 
+
+--------------------------------
+базовые эффекты
 
 
+$("#div1").hide() - display:none
+$("#div1").show() - display:block
 
+fadeOut() - opacity:1
+fadeIn() - opacity: 0
 
+fadeTo(1000,0.5);
 
+fadeToggle(); - если скрыт отобразит и наобарот
 
+slideUp() - сворачивание
+slideDown(); - разворачивает - изменяют высоту 
 
+slideToggle(500) -- как fadeToggle()
 
+#div($).someDiv*5+#modalDiv
 
+у каждого свой цвет
+у #modalDiv display:none
 
+var animTime = 2000;
+$(".someDiv").on('click', function(e) {
+	var modal = $('#modalDiv');
+	modal.css("top", (window.innerHeight - modal.height()) /2); //вычислим центра окна для нашего элемента
+	modal.css("left", (window.innerWidth - modal.width()) /2);
 
+	modal.css("background-color" , $(e.target).css("background-color"));//изменяет цвет в соответвии с кликом на блок
 
+	modal.show(animTime);
+});
+$("#modalDiv").on('click', function(e) {
+	$(this).hide(animTime);
+});
 
+----------------------------------
+анимации
 
+$('selector')
+	.animate({"height": 400},1000)
+	.animate({"width": 400},2000)
+	.animate({'top': (window.innerHeight - 400) / 2 }, 1000);
+	//все будет исполнять по очереди
+	//если хотим убрать очередь то вот вариант
+$('selector')
+	.animate({"height": 400},1000)
+	.animate({"width": 400},{
+		duration:2000,
+		queue: false
+	})
+	.animate({'top': (window.innerHeight - 400) / 2 }, 1000);
+
+//объединение анимаций
+modal
+	.animate({"height": 400,"width": 400},1000)
+	.animate({"top": (window.innerHeight - 400)/2,
+			  'left':(window.innerWidth - 400)/2},1000);
+
+//вторым аргументом animate это объект
+{
+	duration: 2000,
+	queue : false,
+	specialEasing : {
+		height: "linear", //дефаульт
+		width: "swing"//вначале быстро потом медленно
+	}
+}
+//третий аргумент это функция, которая будет выполнена по завершению
+complete: function() {
+	console.log("Animation completed!");
+}
+//четвертый аргумент - функция которая будет выполняться после каждого шага анимации
+step : function() {
+	console.log(n++)//изначально 0, посчитает все кадры анимации
+	if(n === 100) {
+		modal.stop() //остановит анимацию на 100 кадре
+	}
+}
 
 
 
